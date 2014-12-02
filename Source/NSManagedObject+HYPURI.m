7475cd1b503c561a9702902164d6845f33e386cb
Elvis Nuñez <NSElvis@users.noreply.github.com>
2014-12-02 15:09:04 +0100

    Add method to retrieve object using an URI

--- Source/NSManagedObject+HYPURI.m
+++ Source/NSManagedObject+HYPURI.m
@@ -7,4 +7,16 @@
     return [[self.objectID URIRepresentation] absoluteString];
 }
 
++ (NSManagedObject *)managedObjectWithURI:(NSString *)URI inContext:(NSManagedObjectContext *)context
+{
+    NSManagedObjectID *objectID = [context.persistentStoreCoordinator managedObjectIDForURIRepresentation:[NSURL URLWithString:URI]];
+    if (!objectID) return nil;
+
+    NSError *error = nil;
+    NSManagedObject *object = [context existingObjectWithID:objectID error:&error];
+    if (error) NSLog(@"ERROR getting object with URI %@ from MOC:\n%@", URI, [error localizedDescription]);
+
+    return object;
+}
+
 @end
